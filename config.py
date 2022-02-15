c73a9abf389d88553a848156d3761ef407bae400
isaias_pruebas <isgb_codigo@hotmail.com>
2022-02-15 00:19:11 -0600

    Agregando kata 10

---
+++ config.py
@@ -0,0 +1,86 @@
+
+def main():
+    try:
+        configuration = open('config.txt')
+    except FileNotFoundError:
+        print("Couldn't find the config.txt file!")
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+def main():
+    try:
+        configuration = open('config.txt')
+    except Exception:
+        print("Couldn't find the config.txt file!")
+
+def main():
+    try:
+        configuration = open('config.txt')
+    except FileNotFoundError:
+        print("Couldn't find the config.txt file!")
+    except IsADirectoryError:
+        print("Found config.txt but it is a directory, couldn't read it")
+
+def main():
+    try:
+        configuration = open('config.txt')
+    except FileNotFoundError:
+        print("Couldn't find the config.txt file!")
+    except IsADirectoryError:
+        print("Found config.txt but it is a directory, couldn't read it")
+    except (BlockingIOError, TimeoutError):
+        print("Filesystem under heavy load, can't complete reading configuration file")
+
+try:
+     open("config.txt")
+except OSError as err:
+     if err.errno == 2:
+         print("Couldn't find the config.txt file!")
+     elif err.errno == 13:
+         print("Found config.txt but couldn't read it")
+
+
+try:
+     open("mars.jpg")
+
+except FileNotFoundError as err:
+     print("got a problem trying to read the file:", err)
+
+
+try:
+    open("config.txt")
+except OSError as err:
+     if err.errno == 2:
+         print("Couldn't find the config.txt file!")
+     elif err.errno == 13:
+        print("Found config.txt but couldn't read it")
+
+
+def water_left(astronauts, water_left, days_left):
+    daily_usage = astronauts * 11
+    total_usage = daily_usage * days_left
+    total_water_left = water_left - total_usage
+    return f"Total water left after {days_left} days is: {total_water_left} liters"
+
+def water_left(astronauts, water_left, days_left):
+    daily_usage = astronauts * 11
+    total_usage = daily_usage * days_left
+    total_water_left = water_left - total_usage
+    if total_water_left < 0:
+        raise RuntimeError(f"There is not enough water for {astronauts} astronauts after {days_left} days!")
+    return f"Total water left after {days_left} days is: {total_water_left} liters"
+
+
+try:
+    water_left(5, 100, 2)
+except RuntimeError as err:
+    alert_navigation_system(err)
+
+try:
+    water_left(5, 100, 2)
+except RuntimeError as err:
+    alert_navigation_system(err)
